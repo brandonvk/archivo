ead89cb879f02947cb2fafd4e3da31fb99096ccc
Brandon <brandon.aquainteractive@gmail.com>
2018-05-21 00:45:06 -0500

    se modifico la estructura de SQL & se agregaron los ruteadores de las acciones en turno

--- class/Turno.php
+++ class/Turno.php
@@ -1,8 +1,35 @@
 <?php
-  class Turno{
+include_once("includes/Sql.php");
+  class Turno extends Sql{
+
+    public $response;
+
+    public function __construct($params){
+      $this->response = ["success"=>0,"error"=>"ocurrio un error inneserado"];
+      if(!isset($params["action"])) $this->response = ["error"=>"no se encontro accion",'success'=>false];
+      switch ($params["action"]) {
+        case 'getRows':
+        if(!isset($params['page'])) $this->response = ["error"=>"no se encontro página",'success'=>false];
+          $this->get($params['page']);
+          break;
+
+        default:
+          $this->response = ["error"=>"acción no seteada",'success'=>false];
+          break;
+      }
+    }
     public function addTurno(){
-      $params=$_POST;
-      
+
+    }
+    public function get($page){
+      $maxlength=$this->toArray("SELECT count(*) FROM turno;");
+      $rows=$this->toArray("SELECT * FROM turno;");
+      $this->response = [
+        "success"=>1,
+        "rows"=>$rows,
+        "maxlength"=>$maxlength];
     }
   }
+  $Turno = (new Turno($_POST))->response;
+  echo json_encode([$_POST,$Turno]);
 ?>
--- class/includes/Sql.php
+++ class/includes/Sql.php
@@ -2,18 +2,24 @@
 include_once("Credentials.php");
 class Sql extends Credentials{
   private $sql;
-  public function __construct(){
-    print_r([$this->host,$this->user,$this->pswd,$this->bd]);
+
+  public function connect(){
     $this->sql = new mysqli($this->host,$this->user,$this->pswd,$this->bd);
   }
-  public function toArray($query){
 
+  public function dissconect(){
+    mysql_close($this->sql);
+  }
+
+  public function toArray($query){
+    $this->connect();
     $rows=$this->sql->query($query);
 
     $result = [];
     while($row = $rows->fetch_assoc()){
       $result[]=$row;
     }
+    $this->dissconnect();
     return $result;
   }
 }
