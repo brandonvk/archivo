997b76f5002d98c97767feccafe8e88787c16c6c
brandon <brandon.aquainteractive@gmail.com>
2018-05-22 17:01:23 -0500

    se agregaron librerias de iconos & se agrego la funcion de editar & se agrego la opcion de eliminar

--- class/Turno.php
+++ class/Turno.php
@@ -19,6 +19,14 @@ include_once("includes/Sql.php");
             unset($params["action"]);
             $this->addTurno($params);
           break;
+          case 'edit':
+            unset($params["action"]);
+            $this->editTurno($params);
+          break;
+          case 'delete':
+            unset($params["action"]);
+            $this->deleteTurno($params);
+          break;
           default:
             $this->response = ["error"=>"acción no seteada",'success'=>false];
             break;
@@ -36,9 +44,41 @@ include_once("includes/Sql.php");
         $this->response = ["success"=>0,"err"=>"Ocurrio un error al insertar el registro ."];
       }
     }
+
+    public function editTurno($params){
+      if(isset($params["id_turno"])){
+        $params["fecha_entrega"]="{$params["aaaa"]}-{$params["mm"]}-{$params["dd"]}";
+        unset($params["aaaa"]);
+        unset($params["mm"]);
+        unset($params["dd"]);
+        $idTurno=$params["id_turno"];
+        unset($params["id_turno"]);
+
+        if($this->update("turno",$params," id_turno=$idTurno ")){
+          $this->response=["success"=>1,"msg"=>"Se actualizo correctamente el registro."];
+        }else $this->response=["success"=>0,"err"=>"Ocurrio un error al actualizar el registro."];
+      }else $this->response=["success"=>0,"err"=>"No se agrego identificador de turno."];
+    }
+    public function deleteTurno($params){
+      if(isset($params["id"])){
+        $idTurno=$params["id"];
+        unset($params["id"]);
+
+        if($this->update("turno",["isDelete"=>1]," id_turno=$idTurno ")){
+          $this->response=["success"=>1,"msg"=>"Se elimino correctamente el registro."];
+        }else $this->response=["success"=>0,"err"=>"Ocurrio un error al eliminar el registro."];
+      }else $this->response=["success"=>0,"err"=>"No se agrego identificador de turno."];
+    }
     public function get($page){
       $maxlength=$this->toFirstArray("SELECT count(*) as total FROM turno;")["total"];
-      $rows=$this->toArray("SELECT * FROM turno;");
+      // $rows=$this->toArray("SELECT * FROM turno;");
+      // $rows=$this->toArray("SELECT entrega as 'Persona que entrego',
+      //   tipo_documento as 'Tipo de documento',
+      //   fecha_entrega as 'Fecha de entrega',
+      //   descripcion as 'Descripción'
+      //   FROM turno WHERE isDelete=0;");
+      $rows=$this->toArray("SELECT id_turno,fecha_entrega,entrega,tipo_documento,descripcion,turnado
+        FROM turno WHERE isDelete=0;");
       $this->response = [
         "success"=>1,
         "rows"=>$rows,
--- class/includes/Sql.php
+++ class/includes/Sql.php
@@ -3,6 +3,8 @@ include_once("Credentials.php");
 class Sql extends Credentials{
   private $sql;
 
+  protected $getSql='';
+
   public function connect(){
     $this->sql = new mysqli($this->host,$this->user,$this->pswd,$this->bd);
   }
@@ -20,14 +22,26 @@ class Sql extends Credentials{
       $valores.="'$campo',";
     }
     // return "INSERT INTO $table (".substr($campos,0,-1).") values (".substr($valores,0,-1).")";
-    $rs=$this->sql->query("INSERT INTO $table (".substr($campos,0,-1).") values (".substr($valores,0,-1).")");
+    $rs=$this->sql->query($this->getSql="INSERT INTO $table (".substr($campos,0,-1).") values (".substr($valores,0,-1).")");
     $this->dissconect();
     return $rs;
   }
 
+  public function update($table,$data,$filter_id){
+    $this->connect();
+    $campos=[];
+    $valores="";
+    foreach ($data as $key => $campo) {
+      $campos[]="$key='$campo'";
+    }
+    // return "UPDATE $table SET ".implode($campos,",")." WHERE $filter_id;";
+    $rs=$this->sql->query($this->getSql="UPDATE $table SET ".implode($campos,",")." WHERE $filter_id;");
+    $this->dissconect();
+    return $rs;
+  }
   public function toArray($query){
     $this->connect();
-    $rows=$this->sql->query($query);
+    $rows=$this->sql->query($this->getSql=$query);
 
     $result = [];
     while($row = $rows->fetch_assoc()){
